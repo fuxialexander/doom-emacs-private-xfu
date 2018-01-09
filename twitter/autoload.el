44c1ae640b087b89129676f0a2f0fd83cff3b5d4
fuxialexander <fuxialexander@gmail.com>
2018-01-09 11:35:28 +0800

    Add: twittering-mode

---
+++ twitter/autoload.el
@@ -0,0 +1,45 @@
+;;; app/twitter/autoload.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun =twitter ()
+  (interactive)
+  (+workspace-switch "*Twitter*" t)
+  (delete-other-windows)
+  (condition-case _ex
+      (progn
+        (call-interactively #'twit)
+        (unless (get-buffer (car twittering-initial-timeline-spec-string))
+          (error "Failed to open twitter"))
+        (switch-to-buffer (car twittering-initial-timeline-spec-string))
+        (dolist (name (cdr twittering-initial-timeline-spec-string))
+          (split-window-horizontally)
+          (switch-to-buffer name))
+        (balance-windows)
+        (call-interactively #'+twitter/rerender-all))
+    ('error
+     (+twitter/quit-all))))
+
+;;;###autoload
+(defun +twitter/quit ()
+  (interactive)
+  (when (eq major-mode 'twittering-mode)
+    (twittering-kill-buffer)
+    (+workspace/close-window-or-workspace)))
+
+;;;###autoload
+(defun +twitter/quit-all ()
+  (interactive)
+  (+workspace/delete "*Twitter*")
+  (dolist (buf (doom-buffers-in-mode 'twittering-mode (buffer-list)))
+    (with-current-buffer buf
+      (twittering-kill-buffer))))
+
+;;;###autoload
+(defun +twitter/rerender-all ()
+  (interactive)
+  (dolist (buf (doom-buffers-in-mode 'twittering-mode (buffer-list)))
+    (with-current-buffer buf
+      (twittering-rerender-timeline-all buf t)
+      (setq-local line-spacing 0.2)
+      (goto-line 0 buf)
+      )))
