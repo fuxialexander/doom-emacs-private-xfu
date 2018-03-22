369526d26660e7c0b50bf6c1360d1d7c059baa20
fuxialexander <fuxialexander@gmail.com>
2018-03-22 15:38:19 +0800

    Fix: ivy-hydra ui

--- config.el
+++ config.el
@@ -535,6 +535,72 @@ symbol."
 ;; ** Ivy
 (def-package! counsel-tramp :load-path "~/.doom.d/local/"
   :commands (counsel-tramp))
+(after! lv
+  (defun *lv-window ()
+    "Ensure that LV window is live and return it."
+    (if (window-live-p lv-wnd)
+        lv-wnd
+      (let ((ori (selected-window))
+            buf)
+        (prog1 (setq lv-wnd
+                     (select-window
+                      (let ((ignore-window-parameters t))
+                        (split-window
+                         (frame-root-window) -1 'below))))
+          (if (setq buf (get-buffer " *LV*"))
+              (switch-to-buffer buf)
+            (switch-to-buffer " *LV*")
+            (set-window-hscroll lv-wnd 0)
+            (setq window-size-fixed t)
+            (setq mode-line-format nil)
+            (setq cursor-type nil)
+            (set-window-dedicated-p lv-wnd t)
+            (set-window-fringes lv-wnd 0 0 nil)
+            (set-window-parameter lv-wnd 'no-other-window t))
+          (select-window ori)))))
+  (advice-add 'lv-window :override #'*lv-window))
+(after! ivy-hydra
+  (def-hydra! +ivy@coo (:hint nil :color pink)
+    "
+ Move     ^^^^^^^^^^ | Call         ^^^^ | Cancel^^ | Options^^ | Action _w_/_s_/_a_: %s(ivy-action-name)
+----------^^^^^^^^^^-+--------------^^^^-+-------^^-+--------^^-+---------------------------------
+ _g_ ^ ^ _k_ ^ ^ _u_ | _f_orward _o_ccur | _i_nsert | _c_alling: %-7s(if ivy-calling \"on\" \"off\") _C_ase-fold: %-10`ivy-case-fold-search
+ ^|^ _h_ ^+^ _l_ ^I^ | _RET_ done     ^^ | _q_uit   | _m_atcher: %-7s(ivy--matcher-desc) _t_runcate: %-11`truncate-lines
+ _G_ ^ ^ _j_ ^ ^ _d_ | _TAB_ alt-done ^^ | ^ ^      | _<_/_>_: shrink/grow
+"
+    ;; arrows
+    ("j" ivy-next-line)
+    ("k" ivy-previous-line)
+    ("l" ivy-alt-done)
+    ("h" ivy-backward-delete-char)
+    ("g" ivy-beginning-of-buffer)
+    ("G" ivy-end-of-buffer)
+    ("d" ivy-scroll-up-command)
+    ("u" ivy-scroll-down-command)
+    ("e" ivy-scroll-down-command)
+    ;; actions
+    ("q" keyboard-escape-quit :exit t)
+    ("C-g" keyboard-escape-quit :exit t)
+    ("<escape>" keyboard-escape-quit :exit t)
+    ("C-o" nil)
+    ("i" nil)
+    ("TAB" ivy-alt-done :exit nil)
+    ("C-j" ivy-alt-done :exit nil)
+    ("RET" ivy-done :exit t)
+    ("C-m" ivy-done :exit t)
+    ("C-SPC" ivy-call-and-recenter :exit nil)
+    ("f" ivy-call)
+    ("c" ivy-toggle-calling)
+    ("m" ivy-toggle-fuzzy)
+    (">" ivy-minibuffer-grow)
+    ("<" ivy-minibuffer-shrink)
+    ("w" ivy-prev-action)
+    ("s" ivy-next-action)
+    ("a" ivy-read-action)
+    ("t" (setq truncate-lines (not truncate-lines)))
+    ("C" ivy-toggle-case-fold)
+    ("o" ivy-occur :exit t)))
+
 (after! ivy-posframe
   (push '(counsel-rg . nil) ivy-display-functions-alist)
   (push '(counsel-ag . nil) ivy-display-functions-alist)
