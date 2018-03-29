4bf785f245dc142ebc0764c18cff8b3a9207009e
fuxialexander <fuxialexander@gmail.com>
2018-03-29 09:07:13 +0800

    Clean: move magithub filter to gc.org

--- config.el
+++ config.el
@@ -168,34 +168,6 @@ ALPHA : [ %(frame-parameter nil 'alpha) ]
 ;; ** Magit
 (def-package! orgit :after magit)
 (after! magithub
-  (require 'parse-time)
-  (defmacro magithub--time-number-of-days-since-string (iso8601)
-    `(time-to-number-of-days
-      (time-since
-       (parse-iso8601-time-string
-        (concat ,iso8601 "+00:00")))))
-
-  (defun issue-filter-to-days (days type)
-    `(lambda (issue)
-       (let ((created_at (magithub--time-number-of-days-since-string
-                          (alist-get 'created_at issue)))
-             (updated_at (magithub--time-number-of-days-since-string
-                          (alist-get 'updated_at issue))))
-         (or (< created_at ,days) (< updated_at ,days)))))
-
-  (defun magithub-filter-maybe (&optional limit)
-    "Add filters to magithub only if number of issues is greter than LIMIT."
-    (let ((max-issues (length (ignore-errors (magithub-issues))))
-          (max-pull-requests (length (ignore-errors (magithub-pull-requests))))
-          (limit (or limit 1)))
-      (when (> max-issues limit)
-        (add-to-list (make-local-variable 'magithub-issue-issue-filter-functions)
-                     (issue-filter-to-days limit "issues")))
-      (when (> max-pull-requests limit)
-        (add-to-list (make-local-variable 'magithub-issue-pull-request-filter-functions)
-                     (issue-filter-to-days limit "pull-requests")))))
-
-  (add-to-list 'magit-status-mode-hook #'magithub-filter-maybe)
   (setq magithub-clone-default-directory "/Users/xfu/Source/playground/"))
 
 (after! magit
