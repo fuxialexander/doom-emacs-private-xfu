d8ae47e002e19181d7e68200d3341111842e14a7
fuxialexander <fuxialexander@gmail.com>
2018-04-14 20:57:48 +0800

    Add: +org-private/get-name-src-block; move ob-ipython function to autoloads

--- modules/lang/org-private/+babel.el
+++ modules/lang/org-private/+babel.el
@@ -1,67 +1,44 @@
 ;;; lang/org-private/+babel.el -*- lexical-binding: t; -*-
 (add-hook 'org-load-hook #'+org-private|init-babel t)
 (setq +org-babel-mode-alist
-  '(("jupyter-r" . ipython)
-    ("zsh" . shell)
-    ("bash" . shell)
-    ("sh" . shell)))
+      '(("jupyter-r" . ipython)
+        ("zsh" . shell)
+        ("bash" . shell)
+        ("sh" . shell)))
 
 (def-package! ob-ipython
   :after (org ob)
-  :config (setq ob-ipython-resources-dir ".ob-ipython-resrc/"
-                ;; ob-ipython-client-program "/Users/xfu/.emacs.d/.local/packages/elpa/ob-ipython-20180224.153/client.py"
-                )
-  (defun ob-ipython-remote-get-local-path (session host)
-    "Get new config path."
-    (concat (substring (shell-command-to-string (concat "jupyter --runtime-dir")) 0 -1) "/" host "-" session))
-  (defun ob-ipython-server--candidates ()
-    "Collect candidates for remote servers."
-    (let ((source (split-string
-                   (with-temp-buffer
-                     (insert-file-contents "~/.ssh/config")
-                     (buffer-string))
-                   "\n"))
-          (hosts (list)))
-      (dolist (host source)
-        (when (string-match "[H\\|h]ost +\\(.+?\\)$" host)
-          (setq host (match-string 1 host))
-          (if (string-match "[ \t\n\r]+\\'" host)
-              (replace-match "" t t host))
-          (if (string-match "\\`[ \t\n\r]+" host)
-              (replace-match "" t t host))
-          (unless (string= host "*")
-            (push host hosts))))
-      hosts))
-  (set! :popup "\\*remote-.*"
-        '((slot . -1) (side . right) (size . 80))
-        '((select . t) (quit . nil)))
-  (defun ob-ipython-generate-local-path-from-remote (session host)
-    "Copy remote config to local, start a jupyter console to generate a new one."
-    (interactive
-     (list (concat "kernel-" (let ((select-enable-clipboard t)) (current-kill 0)) ".json")
-           (ivy-read "Host: " (ob-ipython-server--candidates))))
-    (let* ((runtime-dir (substring (shell-command-to-string
-                                    (concat "ssh " host " jupyter --runtime-dir")) 0 -1))
-           (runtime-file (concat runtime-dir "/" session))
-           (test (message runtime-file))
-           (tramp-path (concat "/ssh:" host ":" runtime-file))
-           (tramp-copy (ob-ipython-remote-get-local-path session host))
-           (local-path (concat (file-name-sans-extension (file-name-nondirectory tramp-copy)) "-ssh.json")))
-      ;; scp remote file to local
-      (copy-file tramp-path tramp-copy t)
-      ;; connect to remote use new config
-
-      (let ((python-shell-interpreter-interactive-arg " console --simple-prompt"))
-        (get-buffer-process
-         (python-shell-make-comint
-          (concat ob-ipython-command " console --simple-prompt --existing " tramp-copy " --ssh " host)
-          (concat "remote-" session) t)))
-      ;; (start-process (concat "remote-" session)
-      ;;                ob-ipython-command "console" "--simple-prompt" "--existing" tramp-copy "--ssh" host)
-      )))
-
+  :config (setq ob-ipython-resources-dir ".ob-ipython-resrc/")
+  (defvar jupyter-local-runtime-dir (substring (shell-command-to-string (concat "jupyter --runtime-dir")) 0 -1))
+  (advice-add 'org-babel-edit-prep:ipython :override #'*org-babel-edit-prep:ipython)
+  (advice-add 'org-babel-ipython-initiate-session :override #'*org-babel-ipython-initiate-session)
+  (advice-add 'ob-ipython--create-repl :override #'*ob-ipython--create-repl)
+  (advice-add 'org-babel-execute:ipython :override #'*org-babel-execute:ipython)
+  (set! :popup "\\*ob-ipython.*"
+    '((slot . 2) (side . right) (size . 100) (window-height . 0.2))
+    '((select) (quit) (transient)))
+  (set! :popup "\\*Python:.*"
+    '((slot . 0) (side . right) (size . 100))
+    '((select) (quit) (transient))))
 
 (defun +org-private|init-babel ()
+  (def-hydra! +org-private@org-babel-hydra (:color pink :hint nil)
+    "
+Org-Babel: _j_/_k_ next/prev   _g_oto     _TAB_/_i_/_I_ show/hide
+           _'_ edit   _c_lear result      _e_xecute     _s_plit"
+    ("c" org-babel-remove-result)
+    ("e" org-babel-execute-src-block)
+    ("'" org-edit-src-code)
+    ("TAB" org-hide-block-toggle-maybe)
+    ("s" org-babel-demarcate-block)
+    ("g" org-babel-goto-named-src-block)
+    ("i" org-show-block-all)
+    ("I" org-hide-block-all)
+    ("j" org-babel-next-src-block)
+    ("k" org-babel-previous-src-block)
+    ("q" nil "cancer" :color blue))
   (setq org-src-preserve-indentation nil
         org-list-description-max-indent 5
-        org-edit-src-content-indentation 0))
+        org-edit-src-content-indentation 0)
+  (ivy-add-actions '+org-private/get-name-src-block
+                   '(("g" org-babel-goto-named-src-block "Goto"))))
---
+++ modules/lang/org-private/autoload/org-babel.el
@@ -0,0 +1,105 @@
+;;; lang/org-private/autoload/org-babel.el -*- lexical-binding: t; -*-
+
+;; * name
+;;;###autoload
+(defun +org-private/get-name-src-block ()
+  (interactive)
+  (let ((completion-ignore-case t)
+        (case-fold-search t)
+        (all-block-names (org-babel-src-block-names)))
+    (ivy-read "Named Source Blocks: " all-block-names
+              :require-match t
+              :history 'get-name-src-block-history
+              :preselect (let (select (thing-at-point 'symbol))
+                           (if select (substring-no-properties select)))
+              :caller '+org-private/get-name-src-block
+              :action #'+org-private/get-name-src-block-action-insert)))
+
+;;;###autoload
+(defun +org-private/get-name-src-block-action-insert (x)
+  (insert (concat "<<" x ">>\n")))
+
+;; * ob-ipython
+;;;###autoload
+(defun *org-babel-ipython-initiate-session (&optional session params)
+    "Create a session named SESSION according to PARAMS."
+    (if (string= session "none")
+        (error "ob-ipython currently only supports evaluation using a session.
+Make sure your src block has a :session param.")
+      (when (not (s-ends-with-p ".json" session))
+        (ob-ipython--create-kernel (ob-ipython--normalize-session session)
+                                   (cdr (assoc :kernel params))))
+      (ob-ipython--create-repl (ob-ipython--normalize-session session) params)))
+
+;;;###autoload
+(defun *ob-ipython--create-repl (name &optional params)
+    (let ((cmd (s-join " " (ob-ipython--kernel-repl-cmd name))))
+      (if (string= "default" name)
+          (progn
+            (run-python cmd nil nil)
+            (format "*%s*" python-shell-buffer-name))
+        (if (string-match "^remote-.*ssh.json" name)
+            (when (not (ignore-errors (process-live-p (get-process (format "Python:ob-ipython-%s" name)))))
+              (let* ((remote (s-split "-" name))
+                     (remote-host (nth 1 remote))
+                     (remote-session (nth 3 remote)))
+                (ob-ipython-generate-local-path-from-remote remote-session remote-host params)))
+          (let* ((process-name (format "Python:ob-ipython-%s" name))
+                 (buf (python-shell-make-comint cmd process-name t))
+                 (proc (get-buffer-process process-name))
+                 (dir (cdr (assoc :pydir params))))
+            (if dir (with-current-buffer buf
+                      (setq-local default-directory dir)))
+            (sleep-for 1)
+            (format "*%s*" process-name))))))
+
+;;;###autoload
+(defun *org-babel-execute:ipython (body params)
+    "Execute a block of IPython code with Babel.
+This function is called by `org-babel-execute-src-block'."
+    (message default-directory)
+    (let ((session (cdr (assoc :session params))))
+      (org-babel-ipython-initiate-session session params))
+    (ob-ipython--clear-output-buffer)
+    (if (cdr (assoc :async params))
+        (ob-ipython--execute-async body params)
+      (ob-ipython--execute-sync body params)))
+
+;;;###autoload
+(defun *org-babel-edit-prep:ipython (info)
+    ;; TODO: based on kernel, should change the major mode
+    (ob-ipython--create-kernel (->> info (nth 2) (assoc :session) cdr
+                                    ob-ipython--normalize-session)
+                               (->> info (nth 2) (assoc :kernel) cdr))
+    ;; Support for python.el's "send-code" commands within edit buffers.
+    (setq-local python-shell-buffer-name
+                (format "Python:ob-ipython-%s" (->> info (nth 2) (assoc :session) cdr
+                                                    ob-ipython--normalize-session)))
+    (setq-local
+     default-directory (format "%s" (->> info (nth 2) (assoc :pydir) cdr
+                                         ob-ipython--normalize-session)))
+    (ob-ipython-mode 1))
+
+;;;###autoload
+(defun ob-ipython-generate-local-path-from-remote (session host params)
+    "Copy remote config to local, start a jupyter console to generate a new one."
+    (let* ((runtime-dir (substring (shell-command-to-string
+                                    (concat "ssh " host " jupyter --runtime-dir")) 0 -1))
+           (runtime-file (concat runtime-dir "/" "kernel-" session ".json"))
+           (tramp-path (concat "/ssh:" host ":" runtime-file))
+           (tramp-copy (concat jupyter-local-runtime-dir "/remote-" host "-kernel-" session ".json"))
+           (local-path (concat "Python:ob-ipython-" (file-name-sans-extension (file-name-nondirectory tramp-copy)) "-ssh.json")))
+      ;; scp remote file to local
+      (copy-file tramp-path tramp-copy t)
+      ;; connect to remote use new config
+      (let* ((python-shell-interpreter-interactive-arg " console --simple-prompt")
+             (python-shell-completion-native-enable nil)
+             (buf (python-shell-make-comint
+                   (concat ob-ipython-command " console --simple-prompt --existing " tramp-copy " --ssh " host)
+                   (concat "" local-path) t))
+             (proc (get-buffer-process buf))
+             (dir (cdr (assoc :pydir params))))
+        (sleep-for 3)
+        (if dir (with-current-buffer buf
+                  (setq-local default-directory dir)))
+        (format "*%s*" proc))))
