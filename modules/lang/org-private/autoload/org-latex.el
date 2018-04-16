5714fb7e1a2ea431479d0f274e6f293b90f21c85
fuxialexander <fuxialexander@gmail.com>
2018-04-17 02:28:22 +0800

    Add: webkit-katex-render to math mode

--- modules/lang/org-private/autoload/org-latex.el
+++ modules/lang/org-private/autoload/org-latex.el
@@ -6,4 +6,7 @@
   (interactive)
   (if (bound-and-true-p org-cdlatex-mode)
       (org-cdlatex-mode -1)
-    (org-cdlatex-mode +1)))
+    (org-cdlatex-mode +1))
+  (if (bound-and-true-p webkit-katex-render-mode)
+      (webkit-katex-render-mode -1)
+    (webkit-katex-render-mode +1)))
