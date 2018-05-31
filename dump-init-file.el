9c78a9891364727ccbba51badd8c4f3c75db5e4c
fuxialexander <fuxialexander@gmail.com>
2018-05-31 21:37:05 +0800

    Clean: remove redundant configs

--- dump-init-file.el
+++ dump-init-file.el
@@ -1,66 +1,66 @@
-(setq user-emacs-directory (file-name-directory load-file-name)
-      load-prefer-newer noninteractive)
+;; (setq user-emacs-directory (file-name-directory load-file-name)
+;;       load-prefer-newer noninteractive)
 
-(load "~/.emacs.d/.local/packages.el")
-(mapc
- (lambda (x)
-   (condition-case nil
-       (require x)
-     (error nil)))
- '(
-   dash
-   dash-functional
-   f
-   s
-   dired
-   all-the-icons
-   company
-   company-dict
-   magit
-   magit-gitflow
-   magithub
-   eshell
-   neotree
-   smartparens
-   which-key
-   python
-   conda
-   rainbow-delimiters
-   tex-site
-   ess-site
-   elfeed
-   elisp-mode
-   lispy
-   recentf
-   flycheck
-   notmuch
-   markdown-mode
-   ov
-   org
-   org-clock
-   ob
-   ob-ipython
-   ox
-   ox-beamer
-   org-brain
-   posframe
-   yasnippet
-   outline
-   outshine
-   hydra
-   ivy
-   counsel
-   swiper
-   twittering-mode
-   sx
-   lsp-mode
-   lsp-ui
-   avy
-   ace-link
-   ace-window
-   ivy-bibtex
-   biblio
-   ))
+;; (load "~/.emacs.d/.local/packages.el")
+;; (mapc
+;;  (lambda (x)
+;;    (condition-case nil
+;;        (require x)
+;;      (error nil)))
+;;  '(
+;;    dash
+;;    dash-functional
+;;    f
+;;    s
+;;    dired
+;;    all-the-icons
+;;    company
+;;    company-dict
+;;    magit
+;;    magit-gitflow
+;;    magithub
+;;    eshell
+;;    neotree
+;;    smartparens
+;;    which-key
+;;    python
+;;    conda
+;;    rainbow-delimiters
+;;    tex-site
+;;    ess-site
+;;    elfeed
+;;    elisp-mode
+;;    lispy
+;;    recentf
+;;    flycheck
+;;    notmuch
+;;    markdown-mode
+;;    ov
+;;    org
+;;    org-clock
+;;    ob
+;;    ob-ipython
+;;    ox
+;;    ox-beamer
+;;    org-brain
+;;    posframe
+;;    yasnippet
+;;    outline
+;;    outshine
+;;    hydra
+;;    ivy
+;;    counsel
+;;    swiper
+;;    twittering-mode
+;;    sx
+;;    lsp-mode
+;;    lsp-ui
+;;    avy
+;;    ace-link
+;;    ace-window
+;;    ivy-bibtex
+;;    biblio
+;;    ))
 
-(garbage-collect)
-(dump-emacs-portable "~/.emacs.d/.local/doom.pdmp" t)
+;; (garbage-collect)
+;; (dump-emacs-portable "~/.emacs.d/.local/doom.pdmp" t)
--- modules/tools/pdf/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; tools/pdf/packages.el
-
-(package! pdf-tools)
