226fbbd3012c377ca7dc8184067013fa2d6b538d
fuxialexander <fuxialexander@gmail.com>
2018-06-07 20:34:29 +0800

    refactor ob-ipython

--- modules/lang/org-private/+babel.el
+++ modules/lang/org-private/+babel.el
@@ -1,27 +1,12 @@
 ;;; lang/org-private/+babel.el -*- lexical-binding: t; -*-
 (add-hook 'org-load-hook #'+org-private|init-babel t)
 (setq +org-babel-mode-alist
-      '(("jupyter-r" . ipython)
-        ("zsh" . shell)
-        ("bash" . shell)
-        ("sh" . shell)))
+      '((jupyter-r . ipython)
+        (zsh . shell)
+        (bash . shell)
+        (sh . shell)))
 
 (defun +org-private|init-babel ()
-  (defhydra +org-private@org-babel-hydra (:color pink :hint nil)
-    "
-Org-Babel: _j_/_k_ next/prev   _g_oto     _TAB_/_i_/_I_ show/hide
-           _'_ edit   _c_lear result      _e_xecute     _s_plit"
-    ("c" org-babel-remove-result)
-    ("e" org-babel-execute-src-block)
-    ("'" org-edit-src-code)
-    ("TAB" org-hide-block-toggle-maybe)
-    ("s" org-babel-demarcate-block)
-    ("g" org-babel-goto-named-src-block)
-    ("i" org-show-block-all)
-    ("I" org-hide-block-all)
-    ("j" org-babel-next-src-block)
-    ("k" org-babel-previous-src-block)
-    ("q" nil "cancel" :color blue))
   (setq org-src-preserve-indentation nil
         org-list-description-max-indent 5
         org-edit-src-content-indentation 0)
--- modules/lang/org-private/autoload/org-babel.el
+++ modules/lang/org-private/autoload/org-babel.el
@@ -19,3 +19,19 @@
 (defun +org-private/get-name-src-block-action-insert (x)
   (insert (concat "<<" x ">>\n")))
 
+;;;###autoload (autoload '+org-private@org-babel-hydra/body "~/.doom.d/modules/lang/org-private/autoload/org-babel" nil t)
+(defhydra +org-private@org-babel-hydra (:color pink :hint nil)
+    "
+Org-Babel: _j_/_k_ next/prev   _g_oto     _TAB_/_i_/_I_ show/hide
+           _'_ edit   _c_lear result      _e_xecute     _s_plit"
+    ("c" org-babel-remove-result)
+    ("e" org-babel-execute-src-block)
+    ("'" org-edit-src-code)
+    ("TAB" org-hide-block-toggle-maybe)
+    ("s" org-babel-demarcate-block)
+    ("g" org-babel-goto-named-src-block)
+    ("i" org-show-block-all)
+    ("I" org-hide-block-all)
+    ("j" org-babel-next-src-block)
+    ("k" org-babel-previous-src-block)
+    ("q" nil "cancel" :color blue))
