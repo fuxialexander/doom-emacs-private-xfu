6034c34c3e485c5fc095d7dad4ea6d1d307211b6
fuxialexander <fuxialexander@gmail.com>
2018-06-08 14:11:43 +0800

    add tramp multihop through CSE gateway

--- config.el
+++ config.el
@@ -252,6 +252,26 @@
   (setq alert-default-style 'notifier))
 (after! tramp-sh
   (setq tramp-default-method "ssh"
+        ;; this is critical
+        tramp-restricted-shell-hosts-alist '("gw")
+        tramp-default-proxies-alist '(("hpc7" nil "/ssh:gw:")
+                                      ("hpc8" nil "/ssh:gw:")
+                                      ("hpc9" nil "/ssh:gw:")
+                                      ("hpc10" nil "/ssh:gw:")
+                                      ("hpc11" nil "/ssh:gw:")
+                                      ("hpc12" nil "/ssh:gw:")
+                                      ("hpc13" nil "/ssh:gw:")
+                                      ("hpc14" nil "/ssh:gw:")
+                                      ("hpc15" nil "/ssh:gw:")
+                                      ("gpu7" nil "/ssh:gw:")
+                                      ("gpu8" nil "/ssh:gw:")
+                                      ("gpu9" nil "/ssh:gw:")
+                                      ("gpu10" nil "/ssh:gw:")
+                                      ("gpu11" nil "/ssh:gw:")
+                                      ("gpu12" nil "/ssh:gw:")
+                                      ("gpu13" nil "/ssh:gw:")
+                                      ("gpu14" nil "/ssh:gw:")
+                                      ("gpu15" nil "/ssh:gw:"))
         tramp-ssh-controlmaster-options "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=600"
         tramp-remote-process-environment
         (append tramp-remote-process-environment
