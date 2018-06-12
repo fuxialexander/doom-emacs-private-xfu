1d533cf2cbe1aa38929563342153fbd7edec4403
fuxialexander <fuxialexander@gmail.com>
2018-06-13 03:23:43 +0800

    update conda hack

--- modules/lang/python/autoload/conda.el
+++ modules/lang/python/autoload/conda.el
@@ -20,16 +20,23 @@
   "Run `anaconda-mode' server.
 CALLBACK function will be called when `anaconda-mode-port' will
 be bound."
-  (require 'pythonic)
   (setq anaconda-mode-process
-        (start-pythonic :process anaconda-mode-process-name
-                        :buffer anaconda-mode-process-buffer
-                        :filter (lambda (process output) (anaconda-mode-bootstrap-filter process output callback))
-                        :query-on-exit nil
-                        :args (list "-c"
-                                    anaconda-mode-server-command
-                                    (anaconda-mode-server-directory)
-                                    (if (pythonic-remote-p)
-                                        "0.0.0.0" "127.0.0.1")
-                                    (or (pythonic-file-name pythonic-environment) ""))))
-  (process-put anaconda-mode-process 'server-directory (anaconda-mode-server-directory)))
+        (pythonic-start-process :process anaconda-mode-process-name
+                                :buffer anaconda-mode-process-buffer
+                                :query-on-exit nil
+                                :filter (lambda (process output)
+                                          (anaconda-mode-bootstrap-filter process output callback))
+                                :sentinel (lambda (process event))
+                                :args `("-c"
+                                        ,anaconda-mode-server-command
+                                        ,(anaconda-mode-server-directory)
+                                        ,(if (pythonic-remote-p) "0.0.0.0" "127.0.0.1")
+                                        ,(or (pythonic-python-readable-file-name python-shell-virtualenv-root) ""))))
+  (process-put anaconda-mode-process 'interpreter python-shell-interpreter)
+  (process-put anaconda-mode-process 'virtualenv python-shell-virtualenv-root)
+  (when (pythonic-remote-p)
+    (process-put anaconda-mode-process 'remote-p t)
+    (process-put anaconda-mode-process 'remote-method (pythonic-remote-method))
+    (process-put anaconda-mode-process 'remote-user (pythonic-remote-user))
+    (process-put anaconda-mode-process 'remote-host (pythonic-remote-host))
+    (process-put anaconda-mode-process 'remote-port (pythonic-remote-port))))
