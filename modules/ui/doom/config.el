8bb9a22ce7e7c67e31cd800fed48d07370b9c7e4
fuxialexander <fuxialexander@gmail.com>
2018-06-18 23:42:18 +0800

    fix ui

--- modules/ui/doom/config.el
+++ modules/ui/doom/config.el
@@ -30,7 +30,7 @@
     (setq doom-theme 'doom-one))
   ;; Reload common faces when reloading doom-themes live
   (defun +doom*reload-common (&rest _) (load "doom-themes-common.el" nil t))
-  (advice-add #'doom//reload-theme :before #'+doom*reload-common)
+  (advice-add #'doom/reload-theme :before #'+doom*reload-common)
 
   ;; improve integration w/ org-mode
   (add-hook 'doom-load-theme-hook #'doom-themes-org-config)
