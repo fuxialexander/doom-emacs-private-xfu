0ed01a658dc5960db0141e25839309f8a50141fe
fuxialexander <fuxialexander@gmail.com>
2018-06-29 12:58:40 +0800

    integrate lpy and python-send to py-remote

--- modules/lang/python/autoload/python.el
+++ modules/lang/python/autoload/python.el
@@ -20,3 +20,25 @@
           (python-nav-beginning-of-statement)
           (python-nav-end-of-statement))
          (python-nav-forward-statement))))
+
+;;;###autoload
+(defun py-remote (&optional host session dir show internal)
+  "Connect to the remote-host's dtach session running py."
+  (interactive (if (file-remote-p default-directory)
+                   (list
+                    (file-remote-p default-directory 'host)
+                    (read-from-minibuffer "ipython remote session: ")
+                    (file-remote-p default-directory 'localname) t nil)))
+  (let (proc)
+    (save-excursion
+      (setq proc
+            (get-buffer-process
+             (python-shell-make-comint
+              (concat "dtach -A .dtach-" host "-" session " -z -E -r none python -i")
+              (concat "Python:" host "-" session)
+              show internal))))
+    (when (eq major-mode 'python-mode)
+      (setq-local python-shell-buffer-name (concat "Python:" host "-" session))
+      (setq-local lispy-python-proc
+                  (lispy--python-proc
+                   (concat "Python:" host "-" session))))))
