83555f962ef3a22a28df2d87326283b6d125106f
fuxialexander <fuxialexander@gmail.com>
2018-07-12 20:19:14 +0800

    use a centralized directory on server for .dtach sessions

--- modules/lang/python/autoload/python.el
+++ modules/lang/python/autoload/python.el
@@ -22,24 +22,36 @@
          (python-nav-forward-statement))))
 
 ;;;###autoload
+(defvar +dtach-dir "/ssh:hpc9:/uac/gds/xfu/.dtach-session/"
+  "TODO")
+(defvar +dtach--select-session-history nil)
+(defun +dtach--session-candidates (host session)
+  (append `(,(concat host ":" session "_" (format-time-string "%Y-%m-%dT%H:%M:%S")))
+          (directory-files +dtach-dir nil "hpc.*") '(" ")))
+
+;;;###autoload
+(defun +dtach/select-session (host session)
+  (ivy-read "Select or create a dtach session: " (+dtach--session-candidates host session)
+            :initial-input (concat host " " session)
+            :require-match nil
+            :history +dtach--select-session-history))
+
+;;;###autoload
 (defun run-python-remote (&optional host session dir show internal)
   "Connect to the remote-host's dtach session running py."
   (interactive (if (file-remote-p default-directory)
                    (list
                     (file-remote-p default-directory 'host)
-                    (read-from-minibuffer "ipython remote session: ")
+                    (+dtach/select-session (file-remote-p default-directory 'host) (buffer-name))
                     (file-remote-p default-directory 'localname) t nil)))
-  (let (proc)
+  (when (eq major-mode 'python-mode)
     (save-excursion
-      (setq proc
-            (get-buffer-process
-             (python-shell-make-comint
-              (concat "dtach -A .dtach-" host "-" session " -z -E -r none python -i")
-              (concat "Python:" host "-" session)
-              show internal))))
-    (when (eq major-mode 'python-mode)
-      (setq-local python-shell-buffer-name (concat "Python:" host "-" session))
-      ;; (setq-local lispy-python-proc
-      ;;             (lispy--python-proc
-      ;;              (concat "Python:" host "-" session)))
-      )))
+      (with-current-buffer
+          (python-shell-make-comint
+           (concat "dtach -A ~/.dtach-session/" session " -z -E -r none python -i\n\n")
+           (concat "Python:" session)
+           show internal)
+        (comint-send-input)
+        (if (featurep 'evil)
+            (evil-append 1))))
+    (setq-local python-shell-buffer-name (concat "Python:" session))))
