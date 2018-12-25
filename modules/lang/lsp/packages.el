56a03e89513189acf2d5e44b4e6a5729608babee
xfu <fuxialexander@gmail.com>
2018-12-25 13:14:59 +0000

    update lsp to fit upstream

--- modules/lang/lsp/packages.el
+++ modules/lang/lsp/packages.el
@@ -5,6 +5,6 @@
 (package! lsp-ui)
 (package! lsp-mode)
 (package! company-lsp)
-(package! lsp-javascript-typescript)
+;; (package! lsp-javascript-typescript)
 ;; (package! lsp-css :recipe (:fetcher github :repo "emacs-lsp/lsp-css" :files ("*")))
-(package! lsp-python)
+;; (package! lsp-python)
