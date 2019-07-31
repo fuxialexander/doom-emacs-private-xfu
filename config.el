637303cb360764161a7a8c9fb012740c334e5bfd
fuxialexander <fuxialexander@gmail.com>
2019-07-31 06:30:01 +0000

    Unify fringe hack

--- config.el
+++ config.el
@@ -284,6 +284,18 @@
 (add-to-list 'load-path "~/.doom.d/local/emacs-application-framework.git/")
 (add-hook! 'doom-load-theme-hook :append (load! "+themes"))
 
+;; *** fringe
+(defun remove-fringes ()
+    (set-window-fringes nil 0 0)
+    (set-window-margins nil 0 0))
+
+(after! magit
+  (add-hook 'magit-post-display-buffer-hook #'remove-fringes t)
+  (add-hook! magit-popup-mode-hook #'remove-fringes))
+
+(after! treemacs
+  (add-hook 'treemacs-select-hook #'remove-fringes))
+
 ;; *** pdf-tools
 ;; (after! pdf-view
 ;;   (advice-add 'pdf-view-mouse-set-region :override #'*pdf-view-mouse-set-region))
